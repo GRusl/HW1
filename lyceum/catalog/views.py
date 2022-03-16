e708141c6d9676f565bc1bbfe552c367772c8a93
fhsnd <fhsndxva@gmail.com>
2022-03-17 00:02:07 +0300

    Added http request processing for catalog /catalog/ -> Список элементов /catalog/<1|2> -> Подробно элемент

--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from http import HTTPStatus
+from django.http import HttpResponse
 
-# Create your views here.
+
+def item_list(request):
+    return HttpResponse('Список элементов')
+
+
+def item_detail(request, int_id):
+    if int_id in (1, 2):
+        return HttpResponse('Подробно элемент')
+
+    return HttpResponse(status=HTTPStatus.NOT_FOUND)
