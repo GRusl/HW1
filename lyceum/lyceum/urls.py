b159d3f4fd00320e44f2633290bdf5248bcaee12
fhsnd <fhsndxva@gmail.com>
2022-03-17 00:18:47 +0300

    Added http request processing for about
    Bug fixes

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 
 from homepage import views as homepage_views
+from about import views as about_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage_views.home),
-    path('catalog/', include('catalog.urls'))
+    path('catalog/', include('catalog.urls')),
+    path('about/', about_views.description)
 ]
