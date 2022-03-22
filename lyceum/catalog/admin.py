c997f01871f9697176b118ddc371d8cf3bcb9f9a
fhsnd <fhsndxva@gmail.com>
2022-03-22 23:35:04 +0300

    Bug fixes

--- lyceum/catalog/admin.py
+++ lyceum/catalog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from catalog.models import Item, Tag
+from catalog.models import Item, Tag, Category
 
 admin.site.register(Item)
 admin.site.register(Tag)
+admin.site.register(Category)
--- lyceum/catalog/models.py
+++ lyceum/catalog/models.py
@@ -2,21 +2,34 @@ import string
 
 from django.db import models
 
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 from catalog.validators import MinNumWordsValidator, OccurrenceWordsValidator
 from lyceum.validators import ConsistsValidator
 
 
-class Item(models.Model):
-    name = models.CharField('Название', max_length=150)
-    text = models.TextField('Текст', validators=(MinNumWordsValidator(2),
-                                                 OccurrenceWordsValidator(('превосходно', 'роскошно'))))
-
+class TagBaseModel(models.Model):
     is_published = models.BooleanField('Опубликовано', default=True)
 
-
-class Tag(models.Model):
     slug = models.CharField(max_length=200, unique=True,
                             validators=(ConsistsValidator(set(string.ascii_lowercase + string.ascii_uppercase +
                                                           string.digits + "_-")), ))
 
+    class Meta:
+        abstract = True
+
+
+class Item(models.Model):
     is_published = models.BooleanField('Опубликовано', default=True)
+
+    name = models.CharField('Название', max_length=150)
+    text = models.TextField('Текст', validators=(MinNumWordsValidator(2),
+                                                 OccurrenceWordsValidator(('превосходно', 'роскошно'))))
+
+
+class Tag(TagBaseModel):
+    pass
+
+
+class Category(TagBaseModel):
+    weight = models.IntegerField('Вес', default=100, validators=(MinValueValidator(0), MaxValueValidator(32767)))
