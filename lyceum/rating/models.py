db2ab2c28a8105881ccac583cf79aa2f7234a5cf
fhsnd <fhsndxva@gmail.com>
2022-03-22 23:53:05 +0300

    Added database markup for the rating application Tables are displayed in the admin panel russification

--- lyceum/rating/models.py
+++ lyceum/rating/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Rating(models.Model):
+    star = models.IntegerField('Оценка', choices=((1, 'Ненависть'), (2, 'Неприязнь'), (3, 'Нейтрально'),
+                                                  (4, 'Обожание'), (5, 'Любовь')), null=True)
+
+    class Meta:
+        verbose_name = 'Рейтинг'
+        verbose_name_plural = 'Рейтинги'
