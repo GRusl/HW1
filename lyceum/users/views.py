d473b15292fc46ab23836bf157d36405daae728a
fhsnd <fhsndxva@gmail.com>
2022-03-30 19:11:29 +0300

    Adding html

--- lyceum/users/views.py
+++ lyceum/users/views.py
@@ -1,21 +1,23 @@
 from http import HTTPStatus
 from django.http import HttpResponse
 
+from django.shortcuts import render
+
 
 def user_list(request):
-    return HttpResponse('Список пользователей')
+    return render(request, 'users/user_list.html')
 
 
 def user_detail(request, int_id):
     if int_id in (1, ):
-        return HttpResponse('Информация о пользователе')
+        return render(request, 'users/user_detail.html')
 
     return HttpResponse(status=HTTPStatus.NOT_FOUND)
 
 
 def signup(request):
-    return HttpResponse('Регистрация')
+    return render(request, 'users/signup.html')
 
 
 def profile(request):
-    return HttpResponse('Мой профиль')
+    return render(request, 'users/profile.html')
