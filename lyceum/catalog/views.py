536e5591e88736d211cb22f134716d2cb0c434be
fhsnd <fhsndxva@gmail.com>
2022-03-30 18:49:14 +0300

    Adding html

--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -1,13 +1,15 @@
 from http import HTTPStatus
 from django.http import HttpResponse
 
+from django.shortcuts import render
+
 
 def item_list(request):
-    return HttpResponse('Список элементов')
+    return render(request, 'catalog/item_list.html')
 
 
 def item_detail(request, int_id):
     if int_id in (1, 2):
-        return HttpResponse('Подробно элемент')
+        return render(request, 'catalog/item_detail.html')
 
     return HttpResponse(status=HTTPStatus.NOT_FOUND)
