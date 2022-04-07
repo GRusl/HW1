cb4ecf58c2242f32fddb69786bc15494051c0ba2
fhsnd <fhsndxva@gmail.com>
2022-04-07 22:07:26 +0300

    Beautiful views.py

--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -5,15 +5,15 @@ from catalog.models import Item, Tag
 
 
 def item_list(request):
-    context = {
-        'items': Item.objects.get_published().only('name', 'text').prefetch_related(
-            Prefetch('tags', queryset=Tag.objects.get_published().only('name')))
-    }
+    items = Item.objects.get_published().only('name', 'text').prefetch_related(
+        Prefetch('tags', queryset=Tag.objects.get_published().only('name')))
 
+    context = {'items': items}
     return render(request, 'catalog/item_list.html', context=context)
 
 
 def item_detail(request, int_id):
-    context = {'item': get_object_or_404(Item, id=int_id)}
+    item = get_object_or_404(Item, id=int_id)
 
+    context = {'item': item}
     return render(request, 'catalog/item_detail.html', context=context)
--- lyceum/homepage/views.py
+++ lyceum/homepage/views.py
@@ -9,11 +9,9 @@ from catalog.models import Item, Tag
 def home(request):
     items = Item.objects.get_published().only('name', 'text').prefetch_related(
         Prefetch('tags', queryset=Tag.objects.get_published().only('name')))
-
     if len(items) > 3:
         items_id = random.sample(list(items.values_list('id', flat=True)), 3)
         items = Item.objects.filter(id__in=items_id)
 
     context = {'items': items}
-
     return render(request, 'homepage/home.html', context=context)
