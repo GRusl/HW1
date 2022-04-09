b4fb208994f14be61a4a40fa84c71da65419b8d4
Gruslan <ffhshdxva@gmail.com>
2022-04-09 09:18:46 +0300

    Bug fixes

--- lyceum/homepage/views.py
+++ lyceum/homepage/views.py
@@ -9,7 +9,7 @@ def home(request):
     items = Item.objects.get_name_text_tags__name()
     if items.count() > 3:
         items_id = random.sample(list(items.values_list('id', flat=True)), 3)
-        items = Item.objects.filter(id__in=items_id)
+        items = items.filter(id__in=items_id)
 
     context = {'items': items}
     return render(request, 'homepage/home.html', context=context)
