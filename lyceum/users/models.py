c15b8b72f27f2a7ac8ed486a5b14dc883b8f4ed9
Q <fhsndxva@gmail.com>
2022-04-19 23:27:08 +0300

    The birthday field has been added to the existing user model (OneToOneField)

--- lyceum/users/models.py
+++ lyceum/users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthday = models.DateField('День рождения', null=True)
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
+    def __str__(self):
+        return 'День рождения'
