89eaf244adf5e4f19dbbe72270a6c73ff1318266
GRusl <ffhshdxva@gmail.com>
2022-04-27 20:50:38 +0300

    To get "birthday_people_email", the time on the server is used

--- lyceum/users/context_processors.py
+++ lyceum/users/context_processors.py
@@ -1,12 +1,11 @@
-from datetime import datetime
-
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 
 def get_birthday_people_email(request):
     birthday_people_email = (
         get_user_model()
-        .objects.filter(profile__birthday=datetime.now().date())
+        .objects.filter(profile__birthday=timezone.now())
         .only("email")
     )
     return {"birthday_people_email": birthday_people_email}
